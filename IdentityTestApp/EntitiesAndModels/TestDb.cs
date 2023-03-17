88f029461bed66258753c7b487830d2a1c178f29
samyush <samyushmaharjan@gmail.com>
2023-03-17 19:48:47 +0545

    adding foreign key relations

--- IdentityTestApp/EntitiesAndModels/TestDb.cs
+++ IdentityTestApp/EntitiesAndModels/TestDb.cs
@@ -1,7 +1,13 @@
-﻿namespace IdentityTestApp.EntitiesAndModels;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityTestApp.EntitiesAndModels;
 
 public class TestDb
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+    
+    [ForeignKey("Name")]
+    public IdentityUser IdentityUser { get; set; }
 }
